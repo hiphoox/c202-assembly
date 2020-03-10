2705937b8a8dc8d87ec0b8d4e0d5482b624761ad
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-09 18:17:01 -0600

    renamed function name

--- assembly/lib/reader.ex
+++ assembly/lib/reader.ex
@@ -1,10 +1,10 @@
 defmodule Reader do
     def load(file_path) do
         File.read!(file_path)
-        |> get_scs()
+        |> generate_scs()
     end
 
-    def get_scs(file_content)
+    def generate_scs(file_content)
         String.trim(content)
     end
 end
