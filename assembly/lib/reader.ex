69ad6f424123aa8b9a5213213ac043fb4e758ddd
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-09 18:18:10 -0600

    fixed minor bugs

--- assembly/lib/reader.ex
+++ assembly/lib/reader.ex
@@ -2,9 +2,10 @@ defmodule Reader do
     def load(file_path) do
         File.read!(file_path)
         |> generate_scs()
+        |> IO.inspect()
     end
 
-    def generate_scs(file_content)
-        String.trim(content)
+    def generate_scs(file_content) do
+        String.trim(file_content)
     end
 end
