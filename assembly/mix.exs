95e2df24ca58045a4cc9b42a759893c0371914ee
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-09 18:01:35 -0600

    added support for command line executable

--- assembly/mix.exs
+++ assembly/mix.exs
@@ -5,6 +5,7 @@ defmodule Assembly.MixProject do
     [
       app: :assembly,
       version: "0.1.0",
+      escript: escript_config(),
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +24,12 @@ defmodule Assembly.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp escript_config do 
+    [
+      main_module: Assembly
     ]
   end
 end
