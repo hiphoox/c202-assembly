890f220f87359e2639d9889d8b9dd5b7e6e4bb45
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-13 02:55:28 -0600

    lexer finds the invalid token

--- assembly/lib/lexer.ex
+++ assembly/lib/lexer.ex
@@ -9,12 +9,17 @@ defmodule Lexer do
     def lex("", gtl, output) when length(gtl)-1 != length(output) do
         :error_token_not_present
     end
-    def lex("", _, output) do
-        output
+    def lex("", _gtl, output) do
+        err = Enum.find(output, fn x -> x.tag == "error" end)
+        if err == nil do
+            output
+        else
+            {:error_invalid_token, err}
+        end
     end
     def lex(string, gtl, output) do
         {token, match} = Enum.reduce_while(gtl, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
-        token = %{token | expression: String.trim(match)}
-        lex(String.replace_prefix(string, match, ""), gtl, output ++ [token])
+        t = %{token | expression: String.trim(match)}
+        lex(String.replace_prefix(string, match, ""), gtl, output ++ [t])
     end
 end
