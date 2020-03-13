552ac03430cb4f0f0cf7b499b4ab46594161853e
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-13 01:34:46 -0600

    updated lexer to begin handling errors

--- assembly/lib/lexer.ex
+++ assembly/lib/lexer.ex
@@ -1,12 +1,16 @@
 defmodule Lexer do
 
-    def tokenize({scs, gtl, _}) do
+    def tokenize({scs, gtl, _gast}) do
         Hps.TokenListProcessor.clean(scs)
         |> lex(gtl)
     end
 
     def lex(string, gtl, output \\ [])
-    def lex("", _, output), do: output
+    def lex("", _, output) do
+        #To-do: implement error handling
+        length(output) |> IO.inspect(label: "output length")
+        output
+    end
     def lex(string, gtl, output) do
         {token, match} = Enum.reduce_while(gtl, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
         if match == :error do
