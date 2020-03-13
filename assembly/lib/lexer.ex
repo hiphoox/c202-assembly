f676e677e598ad7f7016d3b7269e29cf97f0aa12
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-13 02:36:58 -0600

    added partial support for error handling

--- assembly/lib/lexer.ex
+++ assembly/lib/lexer.ex
@@ -6,18 +6,15 @@ defmodule Lexer do
     end
 
     def lex(string, gtl, output \\ [])
+    def lex("", gtl, output) when length(gtl)-1 != length(output) do
+        :error_token_not_present
+    end
     def lex("", _, output) do
-        #To-do: implement error handling
-        length(output) |> IO.inspect(label: "output length")
         output
     end
     def lex(string, gtl, output) do
         {token, match} = Enum.reduce_while(gtl, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
-        if match == :error do
-            token
-        else
-            token = %{token | expression: String.trim(match)}
-            lex(String.replace_prefix(string, match, ""), gtl, output ++ [token])
-        end
+        token = %{token | expression: String.trim(match)}
+        lex(String.replace_prefix(string, match, ""), gtl, output ++ [token])
     end
 end
