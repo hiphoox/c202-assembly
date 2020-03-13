a7f1de35747d0bb9a8de133f370776591447a9f6
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-13 02:37:11 -0600

    finished adding lexer tests

--- assembly/test/lexer_test.exs
+++ assembly/test/lexer_test.exs
@@ -102,27 +102,90 @@ defmodule LexerTest do
         assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == Hps.Lt.update_otl(context[:otl], new_token)
     end
 
-    test "007_S1_Invalid_ReturnNull", context do 
+    test "007_S1_Invalid_ReturnNull" do 
         gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
         scs = """
         int main() {
           return;
          }
         """
-        new_token = %Structs.Token{expression: "return;", pos_x: nil, pos_y: nil, tag: "error"}
-        nl = List.delete_at(context[:otl], 6)
-        new_list = List.delete_at(nl, 6)
-        assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == Hps.Lt.update_otl(new_list, new_token, 5)
+        assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == :error_token_not_present
     end
 
-    test "008_S1_Invalid_ReturnNoFuncName", context do
+    test "008_S1_Invalid_ReturnNoFuncName" do
         gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
         scs = """
         int () {
           return 1;
         }
         """
-        
+        assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == :error_token_not_present
+    end
+
+    test "009_S1_Invalid_ReturnNoParenth" do
+      gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+      scs = """
+      int main) {
+        return 1;
+      }
+      """
+      assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == :error_token_not_present
+    end
+
+    test "010_S1_Invalid_ReturnNoBrack" do
+      gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+      scs = """
+      int main() 
+        return 13;
+      }
+      """
+      assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == :error_token_not_present
+    end
+
+    test "011_S1_Invalid_ReturnNoSpaces" do 
+        gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+        scs = """
+        intmain() {
+          return 44;
+        }
+        """
+        assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == :error_token_not_present
+    end
+
+    test "012_S1_Invalid_ReturnComma", context do 
+      gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+      scs = """
+      int main() {
+        return 2,
+      }
+      """
+      new_token = %Structs.Token{expression: ",", pos_x: nil, pos_y: nil, tag: "error"}
+      assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == Hps.Lt.update_otl(context[:otl], new_token, 7)
+    end
+
+    test "013_S1_Invalid_ReturnCaps", context do
+        gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+        scs = """
+        Int main() {
+          Return 2;
+        }
+        """
+        token1 = %Structs.Token{expression: "Int", pos_x: nil, pos_y: nil, tag: "error"}
+        token2 = %Structs.Token{expression: "Return", pos_x: nil, pos_y: nil, tag: "error"}
+        nl = Hps.Lt.update_otl(context[:otl], token1, 0)
+        new_list = Hps.Lt.update_otl(nl, token2, 5)
+        assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == new_list
+    end
+
+    test "014_S1_Valid_ReturnPrecZero", context do
+      gtl = Reader.generate_gtl(Hps.Lt.get_gtl_content())
+      scs = """
+      int main() {
+        return 007;
+      }
+      """
+      new_token = %Structs.Token{expression: "007", pos_x: nil, pos_y: nil, tag: "literal"}
+      assert Lexer.tokenize({scs |> Reader.generate_scs(), gtl, ""}) == Hps.Lt.update_otl(context[:otl], new_token)
     end
 
 end
