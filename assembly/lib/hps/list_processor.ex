aeda212f5f3477cc428b9e44902bbf421285c3b2
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-13 12:17:51 -0600

    removed clean_scs function

--- assembly/lib/hps/list_processor.ex
+++ assembly/lib/hps/list_processor.ex
@@ -2,15 +2,8 @@ defmodule Hps.TokenListProcessor do
     defp proc_list([]), do: []
     defp proc_list([ head | tail]), do: [ head.expression | proc_list(tail)]
 
-    def clean(scs) do 
-        String.trim(scs)
-        |> String.replace("\n", " ")
-        |> String.replace("\t", " ")
-    end
-
     def generate_list_of_expressions(gtl) do
         proc_list(gtl)
     end
-
-
+    
 end
