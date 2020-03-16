7ab03e6cb1f49e66e21926d2700ab6ecf2894a97
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-16 14:05:11 -0600

    added handling error code

--- assembly/lib/lexer.ex
+++ assembly/lib/lexer.ex
@@ -11,11 +11,17 @@ defmodule Lexer do
             output
         else
             {:error_invalid_token, err}
+            IO.puts("[Lexer Error]: Invalid token: \'#{err.expression}\' in:")
+            IO.puts build_code(output, err) 
+            System.halt(0)
         end
     end
     def lex(string, gtl, output) do
         {token, match} = Enum.reduce_while(gtl, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
         t = %{token | expression: String.trim(match)}
         lex(String.replace_prefix(string, match, ""), gtl, output ++ [t])
+    end
+    def build_code(output, token) do 
+        "\t" <> Enum.join(Enum.map(output, fn x -> if x == token, do: "\u001B[35m" <> x.expression <> " ", else: "\u001B[30m" <>  x.expression <> " "end))
     end
 end
