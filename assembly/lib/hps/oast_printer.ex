903c3f30a11ce800eb0feac6983c654c9e2f0b9e
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-16 08:03:34 -0600

    final oast version

--- assembly/lib/hps/oast_printer.ex
+++ assembly/lib/hps/oast_printer.ex
@@ -3,7 +3,8 @@ defmodule Hps.OASTPrinter do
         IO.puts eat(oast_root, 0)
     end
     def eat(root, num) do 
-        Enum.map(root.children, fn x -> if x.children == [], do: "\n" <> String.duplicate("\t", num + 1) <> "{#{x.tag} => #{x.token.expression}}", else: "\n" <> String.duplicate("\t", num + 1) <> "{#{root.tag} => #{decide(x)}}" <> "#{eat(x, num + 1)}" end)
+        if num == 0, do: IO.puts "{#{root.tag} => {#{decide(root)}}}"
+        Enum.map(root.children, fn x -> if x.children == [], do: "\n" <> String.duplicate("\t", num + 1) <> "{#{x.tag} => #{x.token.expression}}", else: "\n" <> String.duplicate("\t", num + 1) <> "{#{x.tag} => #{decide(x)}}" <> "#{eat(x, num + 1)}" end)
     end
     def decide(node) do 
         if node.token == nil, do: "\''", else: node.token.expression
