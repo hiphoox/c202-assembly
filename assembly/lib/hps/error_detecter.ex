40b6263114834afd50a6d8382922223cb9a203ef
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-16 19:06:56 -0600

    added support for parser error handling

--- assembly/lib/hps/error_detecter.ex
+++ assembly/lib/hps/error_detecter.ex
@@ -1,13 +1,21 @@
 defmodule Hps.ErrorDetecter do
 
-    @reset "\u001B[0m"
-    @red "\u001B[31m"
-    # @purple "\u001B[35m"
-    # @black "\u001B[30m"
-    @bold "\u001B[1m"
+    @styles %{:reset => "\u001B[0m", :bold => "\u001B[1m"}
+    @colors %{:red => "\u001B[31m"}
 
-    def generate_error(token, file_path) do
-        IO.puts(@red <> "** (Lexer Error) invalid token \'" <> @bold <>  @red <> "#{token.expression}" <> @reset <> @red <> "\' in file #{file_path}" <> @reset)
+    def lexer_error(token, file_path) do
+        IO.puts(@colors[:red] <> "** (Lexer Error) invalid token \'" <> @styles[:bold] <>  @colors[:red] <> "#{token.expression}" <> @styles[:reset] <> @colors[:red] <> "\' in file #{file_path}" <> @styles[:reset])
         System.halt(0)
+    end
+
+    def parser_error(result_token, tl, error_cause, file_path) do
+        if result_token === :token_missing_error do
+            IO.puts parser_localized("structure", error_cause.tag, "is missing something", file_path)
+        else
+            IO.puts parser_localized("token", Enum.at(tl,0).expression, "was not accepted", file_path)
+        end
+    end
+    defp parser_localized(element, fault_token, msg, file_path) do 
+        @colors[:red] <> "** (Parser Error) #{element}" <> @styles[:bold] <>  @colors[:red] <> "<#{fault_token}> " <> @styles[:reset] <> @colors[:red] <> "#{msg} in file #{file_path}"
     end
 end
