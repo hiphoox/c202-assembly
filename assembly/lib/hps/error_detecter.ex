31d02c403c6b0a35fa7c2026a8057f2812db0d72
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-03-16 18:48:21 -0600

    added code for lexer error detecting

--- assembly/lib/hps/error_detecter.ex
+++ assembly/lib/hps/error_detecter.ex
@@ -1,3 +1,13 @@
 defmodule Hps.ErrorDetecter do
-    
+
+    @reset "\u001B[0m"
+    @red "\u001B[31m"
+    # @purple "\u001B[35m"
+    # @black "\u001B[30m"
+    @bold "\u001B[1m"
+
+    def generate_error(token, file_path) do
+        IO.puts(@red <> "** (Lexer Error) invalid token \'" <> @bold <>  @red <> "#{token.expression}" <> @reset <> @red <> "\' in file #{file_path}" <> @reset)
+        System.halt(0)
+    end
 end
