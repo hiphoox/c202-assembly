f9766843ea7bd6dcbe700738d5a125a61a3021df
RickyZoneZero <54494448+RickyZoneZero@users.noreply.github.com>
2020-03-26 18:28:52 -0600

    Update and rename code_generator.ex to CodeGenerator.ex

--- assembly/lib/CodeGenerator.ex
+++ assembly/lib/CodeGenerator.ex
@@ -1,10 +1,4 @@
-<<<<<<< HEAD:assembly/lib/CodeGenerator.ex
 defmodule CodeGenerator do
-  #No me deja poner la primera en minúscula
-=======
-defmodule code_generator do
-
->>>>>>> 5aca6f0482a1bcfa84654eea3cd01ed60d963595:assembly/lib/code_generator.ex
   def generate_code(oast) do
     return_node = get_node_with_tag(Enum.at(oast.children, 0), "operation")
     literal_node = get_node_with_tag(return_node, "literal")
