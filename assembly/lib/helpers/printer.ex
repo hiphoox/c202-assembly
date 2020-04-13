24bb0383b322013c7230671df114cb4f8e4029e8
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 17:26:06 -0500

    refactored printing functions

--- assembly/lib/helpers/printer.ex
+++ assembly/lib/helpers/printer.ex
@@ -6,26 +6,13 @@ defmodule Helpers.Printer do
     @styles %{:reset => "#{@unicode_start}0#{@unicode_finish}", :bold => "#{@unicode_start}1#{@unicode_finish}", :underline => "#{@unicode_start}4#{@unicode_finish}"}
     @colors %{:red => "#{@unicode_start}31#{@unicode_finish}"}
 
-    def scs(scs) do 
-        scs 
-        |> IO.inspect(label: "SCS") 
-    end
-    def gtl(gtl) do 
-        gtl 
-        |> IO.inspect(label: "GTL") 
-    end
-    def otl(otl) do 
-        otl 
-        |> IO.inspect(label: "OTL") 
-        
-    end
-    def cg(cg) do 
-        IO.puts("CG")
-        IO.puts(cg)
-    end
-    def oast(oast_root) do
-        IO.puts("OAST")
-        IO.puts(Helpers.ASTTraveler.travel(oast_root, 0))
+    def print_element(label, element, inspect \\ false) do 
+        IO.puts(label)
+        if inspect do  
+            element |> IO.inspect()
+        else
+            IO.puts(element)
+        end
     end
 
     def _print_error(class, fault_element, reason, location) do
