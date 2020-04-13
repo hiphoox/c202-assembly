8e58f46ce070218f5b0eb9adf766b1fc3dad425b
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 17:04:30 -0500

    added support for Helpers.ASTTraveler

--- assembly/lib/helpers/printer.ex
+++ assembly/lib/helpers/printer.ex
@@ -24,14 +24,8 @@ defmodule Helpers.Printer do
         IO.puts(cg)
     end
     def oast(oast_root) do
-        IO.puts eat(oast_root, 0)
-    end
-    def eat(root, num) do 
-        if num == 0, do: IO.puts "{#{root.tag} => {#{decide(root)}}}"
-        Enum.map(root.children, fn x -> if x.children == [], do: "\n" <> String.duplicate("\t", num + 1) <> "{#{x.tag} => #{x.token.expression}}", else: "\n" <> String.duplicate("\t", num + 1) <> "{#{x.tag} => #{decide(x)}}" <> "#{eat(x, num + 1)}" end)
-    end
-    def decide(node) do 
-        if node.token == nil, do: "\''", else: node.token.expression
+        IO.puts("OAST")
+        IO.puts(Helpers.ASTTraveler.travel(oast_root, 0))
     end
 
     def _print_error(class, fault_element, reason, location) do
