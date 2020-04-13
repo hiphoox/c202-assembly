3d593060b279926f6078502f6b769f66657fd542
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 14:41:46 -0500

    added documentation

--- assembly/lib/invoker.ex
+++ assembly/lib/invoker.ex
@@ -1,4 +1,14 @@
 defmodule Invoker do
+    @moduledoc """
+    Generates the assembly output file. 
+    """
+    @doc """
+    Returns a file ```foo.s``` that contains the assembly code of the source input program.
+    
+    ### Specs  
+    ```file``` is the path to the file to be compiled.
+
+    """
     def invoke_gcc(file) do
         bfn = Path.basename(file, ".s")
         System.cmd("gcc", [file, "-o#{bfn}"])
