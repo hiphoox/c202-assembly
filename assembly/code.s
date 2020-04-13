962ac2bda3f460144ff50d01414b5622da7c0df0
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 01:30:04 -0500

    minor update

--- assembly/code.s
+++ assembly/code.s
@@ -1,6 +1,5 @@
-  .section        __TEXT,__text,regular,pure_instructions
-  .p2align        4, 0x90
-  .globl _main
-_main:
-  movl $1,%eax
-  ret
+
+mov rbx, 2
+mov rcx, $rbx
+mov rbx, $rcx
+mov rax, $rbx
