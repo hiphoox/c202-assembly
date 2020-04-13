03ac9ca81c99afb24e24fd01f263215dd6ac0eb4
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 01:59:53 -0500

    added documentation

--- assembly/lib/lexer.ex
+++ assembly/lib/lexer.ex
@@ -1,36 +1,30 @@
 defmodule Lexer do
     @moduledoc """
-        Breaks the source code string (SCS) into an output list of tokens (OTL).
+    Breaks the source code string (SCS) into an output list of tokens (OTL).
     """
 
     @doc """
-        Breaks the source code string (SCS) into an output list of tokens (OTL).
-        ## Specs  
-        ```{source_code_string, general_token_list}``` is a tuple that contains the following elements:
-            + **source_code_string**: source code
-            + **general_token_list**: target language's valid token list. Refer to Reader.read_general_token_list for more information.
-        
-        ## Examples
-        ```
-            iex> otl = Lexer.tokenize({source_code_string, general_token_list})
-        ``` 
+    Returns an output list of tokens (OTL) by breaking the source code string (SCS).
+    ## Specs 
+    ```arg``` is a tuple that contains the following elements:
+    + **source_code_string**: source code string.
+    + **general_token_list**: target language's valid token list. Refer to `Reader.read_code_and_tokens/3` for more information.
     """
     def tokenize({source_code_string, general_token_list}) do
-        lex(source_code_string, general_token_list)
+        lex(source_code_string, general_token_list, [])
     end
 
-    def lex(string, gtl, output \\ [])
-    def lex("", _gtl, output) do
-        err = Enum.find(output, fn x -> x.tag == "error" end)
+    defp lex("", _, output_token_list) do
+        err = Enum.find(output_token_list, fn x -> x.tag == "error" end)
         if err == nil do
-            {output, :ok}
+            {output_token_list, :ok}
         else
             {err, :error}
         end
     end
-    def lex(string, gtl, output) do
-        {token, match} = Enum.reduce_while(gtl, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
+    defp lex(source_code_string, general_token_list, output_token_list) do
+        {token, match} = Enum.reduce_while(general_token_list, {"",""}, fn x, _ -> if (regex_match = Regex.run(~r/^\s*#{x.expression}\s*/, source_code_string)) == nil, do: {:cont, {x, :error}}, else: {:halt,{x, Enum.at(regex_match,0)}} end)
         t = %{token | expression: String.trim(match)}
-        lex(String.replace_prefix(string, match, ""), gtl, output ++ [t])
+        lex(String.replace_prefix(source_code_string, match, ""), general_token_list, output_token_list ++ [t])
     end
 end
