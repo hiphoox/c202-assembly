fc174c234c8984eb3508edd8f8e1012dee02df2e
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 01:32:12 -0500

    added documentation and major refactoring

--- assembly/lib/reader.ex
+++ assembly/lib/reader.ex
@@ -1,42 +1,86 @@
 defmodule Reader do
+    @moduledoc """
+    Reads all the files the compiler needs. 
+    """
     
-    @c_tokens_path "./specification_files/c_tokens.xml"
-    @c_structures_path "./specification_files/c_structures.xml"
+    @doc """
+    Reads the source code from a file path and a general token list from an XML file. 
+    ## Specs  
+    ```source_code_path``` is the path to the file to be compiled.          
+    ```tokens_path``` is the path to an XML file containing the target language's valid token list.
 
-    def load(file_path)                   do
-        scs = File.read!(file_path) |> generate_scs()
-        gtl = File.read!(@c_tokens_path) |> generate_gtl()
-        g = add_error_token(gtl)
-        {scs, g}
+    Generates the following output:
+    + Source Code String (SCS): source code contained in a string.
+    + General Token List (GTL): list of target language valid tokens.
+    
+    ## Examples
+    ```
+        iex> {scs, gtl} = Reader.read_code_and_tokens('examples/test.c', tokens_path)
+    ``` 
+    """
+    def read_code_and_tokens(source_code_path, tokens_path, verbose \\ false) do
+        scs = read_source_code_string(source_code_path)
+        gtl = read_general_token_list(tokens_path)
+        if verbose do 
+            #TODO: print scs and gtl
+        end
+        {scs, gtl}
     end
 
-    def load_gast() do 
-        File.read!(@c_structures_path) |> generate_gast()
+    @doc """
+    Reads the source code as a string.
+    ## Specs  
+    ```source_code_path``` is the path to the file to be compiled.  
+
+    Generates the following output:
+    + Source Code String (SCS): String.
+    
+    ## Examples
+    ```
+        iex> scs = Reader.read_source_code_string(source_code_path)
+    ``` 
+    """
+    def read_source_code_string(source_code_path)           do 
+        File.read!(source_code_path) 
+        |> _generate_source_code_string()
     end
 
-    def add_error_token(gtl)              do
-        gtl ++ [%Structs.Token{tag: "error", expression: "\\S+", pos_y: nil, pos_x: nil}]
+    def _generate_source_code_string(file_content)          do
+        clean_source_code_string(file_content)
     end
 
-    def generate_scs(file_content)        do
+    defp clean_source_code_string(file_content)             do
         String.trim(file_content)
         |> String.replace("\n", " ")
         |> String.replace("\t", " ")
     end
 
-    def generate_gtl(file_content)        do
+    @doc """
+    Reads the structure of the target language's valid tokens as a General Token List (GTL).
+    ## Specs  
+    ```tokens_path``` is the path to an XML file containing the target language's valid token list. 
+
+    Generates the following output:
+    + General Token List (GTL): list of target language's valid tokens.
+    
+    ## Examples
+    ```
+        iex> gtl = Reader.read_general_token_list(tokens_path)
+    ``` 
+    """
+    def read_general_token_list(tokens_path)                do
+        File.read!(tokens_path) 
+        |> _generate_general_token_list()
+    end
+
+    def _generate_general_token_list(file_content)          do
         XmlToMap.naive_map(file_content)
         |> generate_useful_list()
         |> parse_to_token_list()
+        |> add_error_token()
     end
 
-    def generate_gast(file_content)       do
-        XmlToMap.naive_map(file_content) 
-        |> generate_useful_list()
-        |> parse_to_node_list()
-    end
-
-    defp parse_to_token_list(list)        do
+    defp parse_to_token_list(list)                          do
         Enum.map list, fn(token) -> 
             %Structs.Token{
                 tag:        token["tag"],
@@ -47,7 +91,41 @@ defmodule Reader do
         end
     end
 
-    defp parse_to_node_list(list)         do
+    defp add_error_token(gtl)                               do
+        gtl ++ [%Structs.Token{tag: "error", expression: "\\S+", pos_y: nil, pos_x: nil}]
+    end
+
+    @doc """
+    Reads the structure of the target language's grammar production rules from an XML file.
+    ## Specs  
+    ```grammar_prod_rules_path``` is the path to an XML file containing the target language's grammar production rules.
+
+    Generates the following output:
+    + General Abstract Syntax Treee (GAST): list containing the target language grammar production rules.
+    
+    ## Examples
+    ```
+        iex> gast = Reader.read_general_ast(grammar_prod_rules_path)
+    ``` 
+    """
+    def read_general_ast(grammar_prod_rules_path)           do 
+        File.read!(grammar_prod_rules_path) 
+        |> _generate_general_ast()
+    end
+
+    def _generate_general_ast(file_content)                 do
+        XmlToMap.naive_map(file_content) 
+        |> generate_useful_list()
+        |> parse_to_node_list()
+    end
+
+    defp generate_useful_list(map)                          do
+        [general_list | _] = Map.values map
+        [useful_list | _ ] = Map.values general_list
+        useful_list
+    end
+
+    defp parse_to_node_list(list)                           do
         Enum.map list, fn(node) -> 
             %Structs.Node{
                 tag:        node["tag"],
@@ -59,21 +137,16 @@ defmodule Reader do
         end
     end
 
-    def get_c(node) do 
+    defp get_c(node)                                        do 
         if node["substructure"] == nil, do: [], else: node["substructure"]
     end
 
-    def get_t(node) do 
+    defp get_t(node)                                        do 
         if node["token"] == %{}, do: nil, else: node["token"]
     end
 
-    def get_asm(node) do 
+    defp get_asm(node)                                      do 
         if node["asm"] == %{}, do: "", else: node["asm"]
     end
 
-    defp generate_useful_list(map)        do
-        [general_list | _] = Map.values map
-        [useful_list | _ ] = Map.values general_list
-        useful_list
-    end
 end
