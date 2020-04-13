08566b785cdd89c447fa9328caf14e4ef8b3921a
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 02:33:19 -0500

    added basic documentation

--- assembly/lib/structs/token.ex
+++ assembly/lib/structs/token.ex
@@ -1,3 +1,7 @@
 defmodule Structs.Token do
+    @moduledoc """
+    Defines a Token that represents a valid, or invalid, token in the target programming language.
+    """
+
     defstruct [:tag, :expression, :pos_y, :pos_x]
 end
