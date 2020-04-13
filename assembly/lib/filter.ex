0a852a52f2653938d59632e06d0e6376626e3c72
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 02:21:40 -0500

    added HexDoc documentation

--- assembly/lib/filter.ex
+++ assembly/lib/filter.ex
@@ -1,4 +1,20 @@
 defmodule Filter do 
+    @moduledoc """
+    Filters the outputs of `Lexer.tokenize/1` and `Parser.parse/2`. 
+    """
+
+    @doc """
+    ## Specs
+    Given the output of the `Lexer`, it prints any lexing errors or continues with the compilation if there are none.
+
+    ```arg``` is a tuple that contains the following elements:
+    + **output_token_list**: list of all the tokens found in the source code. Refer to `Structs.Token` for more information. 
+    + **status**: atom indicating if there was an invalid token in the source code.
+
+    ```source_code_path``` is the path to the file to be compiled. 
+    
+    ```verbose``` a boolean value indicating if the compiler should output all of its steps.
+    """
     def filter_lexer_output({otl, :error}, source_code_path, _) do 
         Hps.ErrorDetecter.lexer_error(otl, source_code_path)
         System.halt(1)
@@ -10,16 +26,30 @@ defmodule Filter do
         {otl, :ok}
     end
 
-    def filter_parser_output({:token_missing_error, _, tl, error_cause}, file_path, _) do
-        Hps.ErrorDetecter.parser_error(:token_missing_error, tl, error_cause, file_path)
+    @doc """
+    ## Specs
+    Given the output of the `Parser`, it prints any parsing errors or continues with the compilation if there are none.
+
+    ```arg``` is a tuple that contains the following elements:
+    + **status**: atom indicating if there was an invalid token in the source code.
+    + **output_abstract_syntax_tree**: Abstract Syntax Tree (OAST) generated given Output Token List (OTL).
+    + **token_list**: list of all the bad tokens found in the source code
+    + **error_cause**: string containing the cause of the bad tokens.
+    
+    ```source_code_path``` is the path to the file to be compiled. 
+
+    ```verbose``` a boolean value indicating if the compiler should output all of its steps.
+    """
+    def filter_parser_output({:token_missing_error, _, token_list, error_cause}, source_code_path, _) do
+        Hps.ErrorDetecter.parser_error(:token_missing_error, token_list, error_cause, source_code_path)
         System.halt(1)
     end
-    def filter_parser_output({:token_not_absorbed_error, _, tl, error_cause}, file_path, _) do
-        Hps.ErrorDetecter.parser_error(:token_not_absorbed_error, tl, error_cause, file_path)
+    def filter_parser_output({:token_not_absorbed_error, _, token_list, error_cause}, source_code_path, _) do
+        Hps.ErrorDetecter.parser_error(:token_not_absorbed_error, token_list, error_cause, source_code_path)
         System.halt(1)
     end
-    def filter_parser_output({_,oast,_,_}, _, verbose) do
-        Hps.OASTPrinter.print(oast, verbose)
-        oast
+    def filter_parser_output({_,output_abstract_syntax_tree,_,_}, _, verbose) do
+        Hps.OASTPrinter.print(output_abstract_syntax_tree, verbose)
+        output_abstract_syntax_tree
     end
 end
