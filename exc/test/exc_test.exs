0188e197abf0f0fc6e92e7ca4187792d4e5826d8
Alejandro Bondi <alejandrobondi@me.com>
2020-04-14 11:40:30 -0500

    Stage 2 Testing Documentation & Files
    Added stage 2 testing documentation and resource files in order to continue with the development of testing scripts.

--- exc/test/exc_test.exs
+++ exc/test/exc_test.exs
@@ -6,6 +6,7 @@ defmodule ExcTest do
 
     test "001_Valid_Return_0" do
         scs = File.read("../docs/testing_integration/Stage_1/001_S1_Valid_Return0.c")
+        # target result file
         tar = File.read(""../docs/testing_integration/Stage_1/001_S1_Valid_Return0.s"")
 
         Assert exc.Start(scs)
