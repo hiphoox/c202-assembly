9e28e87564787ef1f076dc2d4bb5c7ad23e42648
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-13 22:31:49 -0500

    ...

--- assembly/code.s
+++ assembly/code.s
@@ -1,12 +1,13 @@
-
-
-mov rbx, 2
-
-mov rcx, $rbx
-
-
-
-
-
-mov rbx, $rcx
-mov rax, $rbx
+    .section        __TEXT,__text,regular,pure_instructions
+    .p2align        4, 0x90
+    .globl _main
+  _main: 
+            
+movl 23, %ebx
+neg %ebx
+movl $ebx, %ecx
+movl $ecx, %ebx
+mov $ebx, %ecx
+movl $ecx, %eax
+ 
+    ret
