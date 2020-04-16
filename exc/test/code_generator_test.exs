95826b2378dca07186f53695b24476b92793c967
Alejandro Bondi <alejandrobondi@me.com>
2020-04-16 10:21:07 -0500

    Added Code Generator Files

--- exc/test/code_generator_test.exs
+++ exc/test/code_generator_test.exs
@@ -49,4 +49,36 @@ defmodule CodeGeneratorTest do
         
         assert cg == "    .section        __TEXT,__text,regular,pure_instructions\n    .p2align        4, 0x90\n    .globl _main\n  _main: \n            \nmovl $7, %ebx\nnot %ebx\nmovl %ebx, %ecx\nmovl %ecx, %ebx\nmovl %ebx, %ecx\nmovl %ecx, %eax\n \n    ret\n"
     end
+
+    test "017_S2_Valid_Bitwise0" do
+        verbose = false
+        c_tokens_path = "./specification_files/c_tokens.xml"
+        c_structures_path = "./specification_files/c_structures.xml"
+        file_path = "./examples/stage_2/017_S2_Valid_Bitwise0.c"
+        
+        cg = Reader.read_code_and_tokens(file_path, c_tokens_path, verbose)
+        |> Lexer.tokenize()
+        |> Filter.filter_lexer_output(file_path, verbose)
+        |> Parser.parse(Reader.read_general_ast(c_structures_path))
+        |> Filter.filter_parser_output(file_path, verbose)
+        |> CodeGenerator.generate_code(verbose)
+        
+        assert cg == "    .section        __TEXT,__text,regular,pure_instructions\n    .p2align        4, 0x90\n    .globl _main\n  _main: \n            \nmovl $0, %ebx\nnot %ebx\nmovl %ebx, %ecx\nmovl %ecx, %ebx\nmovl %ebx, %ecx\nmovl %ecx, %eax\n \n    ret\n"
+    end
+
+    test "018_S2_Valid_Not_7" do
+        verbose = false
+        c_tokens_path = "./specification_files/c_tokens.xml"
+        c_structures_path = "./specification_files/c_structures.xml"
+        file_path = "./examples/stage_2/018_S2_Valid_Not_7.c"
+        
+        cg = Reader.read_code_and_tokens(file_path, c_tokens_path, verbose)
+        |> Lexer.tokenize()
+        |> Filter.filter_lexer_output(file_path, verbose)
+        |> Parser.parse(Reader.read_general_ast(c_structures_path))
+        |> Filter.filter_parser_output(file_path, verbose)
+        |> CodeGenerator.generate_code(verbose)
+        
+        assert cg == "    .section        __TEXT,__text,regular,pure_instructions\n    .p2align        4, 0x90\n    .globl _main\n  _main: \n            \nmovl $7, %ebx\nnot %ebx\nmovl %ebx, %ecx\nmovl %ecx, %ebx\nmovl %ebx, %ecx\nmovl %ecx, %eax\n \n    ret\n"
+    end
 end
