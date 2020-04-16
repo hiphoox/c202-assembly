6b48f641d37f27186888fdfe4f4cbc512259dc5c
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-04-15 20:14:34 -0500

    refactored Writer

--- exc/lib/helpers/string_elements.ex
+++ exc/lib/helpers/string_elements.ex
@@ -1,9 +1,13 @@
 defmodule Helpers.StringElements do 
     @moduledoc false
     
-    def scs(), do: "SCS"
-    def gtl(), do: "GTL"
-    def otl(), do: "OTL"
-    def rc(), do: "Raw Code"
-    def oast(), do: "OAST"
+    # Compiler general name modules 
+    def scs(), do: "SCS"        # Source Code String    
+    def gtl(), do: "GTL"        # General Token List
+    def otl(), do: "OTL"        # Output Token List
+    def rc(), do: "Raw Code" 
+    def oast(), do: "OAST"      # Output Abstract Syntax Tree
+
+    # Output file names
+    def asm_generated_file(), do: "exc_o.s"
 end
