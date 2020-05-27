9cecc8b953a5915858b2e039f96f3b5f41158f45
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-05-26 22:44:03 -0500

    added reader tests

---
+++ exc/test/reader_test.exs
@@ -0,0 +1,15 @@
+defmodule ReaderTest do
+  use ExUnit.Case
+
+  test "001_S1_Valid_Return0" do
+      scs = """
+      int main() {
+      \treturn 0;
+      }
+      """
+      assert Reader._generate_source_code_string(scs) == "int main() { return 0; }"
+  end
+
+  
+
+end
