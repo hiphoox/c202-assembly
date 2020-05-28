79381e8ab26c194c6067e8729354cfe9fa9b8dc2
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-05-28 00:52:24 -0500

    added stage 2 reader tests

---
+++ exc/test/reader/stage_2.exs
@@ -0,0 +1,13 @@
+defmodule StageTwoReader do
+  use ExUnit.Case
+
+  test "001_S2_Valid_Negative" do
+      scs = """
+      int main() {
+      \treturn -7;
+      }
+      """
+      assert Reader._generate_source_code_string(scs) == "int main() {  return -7; }"
+  end
+
+end
