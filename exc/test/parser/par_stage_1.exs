2d7308683cb2c55b3dce30511b4cd91c97a7e094
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-05-29 13:06:50 -0500

    started parser testing

--- exc/test/parser/par_stage_1.exs
+++ exc/test/parser/par_stage_1.exs
@@ -23,6 +23,30 @@ defmodule StageOneParser do
           %Structs.Token{expression: "return", pos_x: nil, pos_y: nil, tag: "return"},
           %Structs.Token{expression: ";", pos_x: nil, pos_y: nil, tag: "semicolon"},
           %Structs.Token{expression: "}", pos_x: nil, pos_y: nil, tag: "bracket-close"}
+        ],
+        oast:
+        [
+          %Structs.Node{asm: "movl %:0, %eax", 
+          children: [
+            %Structs.Node{asm: "movl %:5, %:r",
+            children: [
+              %Structs.Node{asm: "", children: [], class: "data-type", tag: "int-data-type", token: %Structs.Token{expression: "int", pos_x: nil, pos_y: nil, tag: "int"}},
+              %Structs.Node{asm: "", children: [], class: "function-name", tag: "main-function-name", token: %Structs.Token{expression: "main", pos_x: nil, pos_y: nil, tag: "main"}},
+              %Structs.Node{asm: "", children: [], class: ["group-opener", "args-opener"], tag: "evaluator-open", token: %Structs.Token{expression: "(", pos_x: nil, pos_y: nil, tag: "parenthesis-open"}},
+              %Structs.Node{asm: "", children: [], class: ["group-closer", "args-closer"], tag: "evaluator-close", token: %Structs.Token{expression: ")", pos_x: nil, pos_y: nil, tag: "parenthesis-close"}},
+              %Structs.Node{asm: "", children: [], class: "function-opener", tag: "section-open", token: %Structs.Token{expression: "{", pos_x: nil, pos_y: nil, tag: "bracket-open"}},
+              %Structs.Node{asm: "movl %:1, %:r", 
+              children: [
+                %Structs.Node{asm: "", children: [], class: "return-word", tag: "return-word", token: %Structs.Token{expression: "return", pos_x: nil, pos_y: nil, tag: "return"}},
+                # %Structs.Node{asm: "mov %:0, %:r",
+                %Structs.Node{asm: "", children: [], class: "line-ender", tag: "semicolon", token: %Structs.Token{expression: ";", pos_x: nil, pos_y: nil, tag: "semicolon"}}
+              ], class: ["operation", "returner"], tag: "operation", token: nil},
+
+              %Structs.Node{asm: "", children: [], class: "function-closer", tag: "section-close", token: %Structs.Token{expression: "}", pos_x: nil, pos_y: nil, tag: "bracket-close"}}
+            ], 
+            class: "program-root", tag: "function", token: nil}
+          ], 
+          class: :root, tag: :root, token: nil}
         ]
       ]
     }
@@ -38,15 +62,11 @@ defmodule StageOneParser do
     )
     general_abstract_syntax_tree =  Helpers.LexerTester.get_c_structures_content()
                                     |> Reader._generate_general_ast()
-    {status_token, output_abstract_syntax_tree, token_list, error_cause} = 
+    {status_token, oast, token_list, error_cause} = 
       Parser.parse(output_token_list, general_abstract_syntax_tree)
 
-    oast_root = output_abstract_syntax_tree
-    class = oast_root.class
-
     # Parser outputs
-    assert [class] == [:root]
-    
+    assert {status_token, oast, token_list, error_cause} == {:ok, List.first(context[:oast]), [], nil}
   end
 
 end
