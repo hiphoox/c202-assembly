04e732d87cb4c0b27035c055ebed66f96a95a2b1
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-05-28 20:02:50 -0500

    added module for testing lexer filter

--- exc/test/filter/lexer/f_lx_stage_1_test.exs
+++ exc/test/filter/lexer/f_lx_stage_1_test.exs
@@ -29,25 +29,25 @@ defmodule StageOneFilterLexer do
       ]}
     end
 
-  test "001_S1_Valid_", context do
-    test_path = "test/filter/lexer/f_lx_stage_1_test.exs"
-    general_token_list =  Helpers.LexerTester.get_c_tokens_content() 
-                          |> Reader._generate_general_token_list()
-    source_code_string = """
-    int main() {
-      return;
-    }
-    """ |> Reader._generate_source_code_string()
-    left_hand_side =  Lexer.tokenize({source_code_string, general_token_list})
-                      |> Filter.filter_lexer_output(test_path, false)
+  # test "001_S1_Valid_", context do
+  #   test_path = "test/filter/lexer/f_lx_stage_1_test.exs"
+  #   general_token_list =  Helpers.LexerTester.get_c_tokens_content() 
+  #                         |> Reader._generate_general_token_list()
+  #   source_code_string = """
+  #   int main() {
+  #     return;
+  #   }
+  #   """ |> Reader._generate_source_code_string()
+  #   left_hand_side =  Lexer.tokenize({source_code_string, general_token_list})
+  #                     |> Filter.filter_lexer_output(test_path, false)
 
-    literal_token = %Structs.Token{expression: "7", pos_x: nil, pos_y: nil, tag: "literal"}
-    token_list = [literal_token]
-    incomplete_output_token_list = context[:output_token_list]
-    output_token_list = Helpers.LexerTester.insert_token_list(incomplete_output_token_list, token_list, 6)
-    right_hand_side = output_token_list
+  #   literal_token = %Structs.Token{expression: "7", pos_x: nil, pos_y: nil, tag: "literal"}
+  #   token_list = [literal_token]
+  #   incomplete_output_token_list = context[:output_token_list]
+  #   output_token_list = Helpers.LexerTester.insert_token_list(incomplete_output_token_list, token_list, 6)
+  #   right_hand_side = output_token_list
 
-    assert left_hand_side == right_hand_side
-  end
+  #   assert left_hand_side == right_hand_side
+  # end
 
 end
