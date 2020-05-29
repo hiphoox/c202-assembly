a24595fc339457df204fbe9798fb9405c9828d7b
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-05-29 13:47:11 -0500

    renamed parameter name

--- exc/lib/invoker.ex
+++ exc/lib/invoker.ex
@@ -9,9 +9,9 @@ defmodule Invoker do
     ```file``` is the path to the file to be compiled.
 
     """
-    def invoke_gcc(assembly_source_code) do
-        target_code_name = Path.basename(assembly_source_code, ".s")
-        System.cmd("gcc", [assembly_source_code, "-o#{target_code_name}"])
-        File.rm!(assembly_source_code)
+    def invoke_gcc(assembly_source_code_path) do
+        target_code_name = Path.basename(assembly_source_code_path, ".s")
+        System.cmd("gcc", [assembly_source_code_path, "-o#{target_code_name}"])
+        File.rm!(assembly_source_code_path)
     end
 end
