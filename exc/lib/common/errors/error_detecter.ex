f5faeac7f8d7a7d8a8be8e1834a0df5b31e45107
nestorivanmo <nestor.martinez.98@hotmail.com>
2020-06-10 13:12:01 -0500

    added functionality to support row col error detecter

--- exc/lib/common/errors/error_detecter.ex
+++ exc/lib/common/errors/error_detecter.ex
@@ -1,13 +1,14 @@
 defmodule Error.ErrorDetecter do
   @moduledoc false
-
   def lexer_error(token, file_path)                                  do
+    {row, col} = Error.RowColDetecter.find_row_col(token, file_path)
     class_msg =         "** (Lexer Error) invalid token"
     fault_element_msg = "#{token.expression}"
     reason_msg  =       ""
     location_msg =      "#{file_path}"
+    row_col_msg =       ":#{row}:#{col}"
     IO.Printer._print_error(
-        class_msg, fault_element_msg, reason_msg, location_msg
+      class_msg, fault_element_msg, reason_msg, location_msg, row_col_msg
     )
   end
 
@@ -16,8 +17,9 @@ defmodule Error.ErrorDetecter do
     fault_element =     error_cause.tag
     reason_msg =        "is missing something"
     location =          file_path
+    row_col_msg =       ""
     IO.Printer._print_error(
-        class_msg, fault_element, reason_msg, location
+      class_msg, fault_element, reason_msg, location, row_col_msg
     )
   end
   def parser_error(:token_not_absorbed_error, tl, _, file_path)      do
@@ -25,8 +27,9 @@ defmodule Error.ErrorDetecter do
     fault_element = Enum.at(tl, 0).expression
     reason_msg =    "was not accepted"
     location =      file_path
+    row_col_msg =   ""
     IO.Printer._print_error(
-        class_msg, fault_element, reason_msg, location
+      class_msg, fault_element, reason_msg, location, row_col_msg
     )
   end
 end
